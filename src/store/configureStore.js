38672f0819eb90242c83d1dca4b2ad0f3ec34b3d
Geordy James <geordy06@gmail.com>
2018-11-11 19:03:04 +0530

    updated read.md file and disable redux dev tool on production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,16 +3,19 @@ import thunk from 'redux-thunk';
 import authReducer from '../reducers/auth';
 import todoReducer from '../reducers/todo';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+	process.env.NODE_ENV != 'production'
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+		: compose;
 
 export default () => {
-  const store = createStore(
-    combineReducers({
-      auth: authReducer,
-      todo: todoReducer
-    }),
-    composeEnhancers(applyMiddleware(thunk))
-  );
+	const store = createStore(
+		combineReducers({
+			auth: authReducer,
+			todo: todoReducer
+		}),
+		composeEnhancers(applyMiddleware(thunk))
+	);
 
-  return store;
+	return store;
 };
